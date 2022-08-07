447fcf025f70409a9ef9b2cbc7083e72fa478187
007 <dr007cc@gmail.com>
2022-08-08 00:09:49 +0430

    feat: add Types

---
+++ src/types/options.ts
@@ -0,0 +1,64 @@
+import { CSSProperties } from 'react'
+
+type PositionsString =
+    | 'top left'
+    | 'top center'
+    | 'top right'
+    | 'middle left'
+    | 'middle'
+    | 'middle right'
+    | 'bottom left'
+    | 'bottom center'
+    | 'bottom right'
+
+enum Positions {
+    TOP_LEFT = 'top left',
+    TOP_CENTER = 'top center',
+    TOP_RIGHT = 'top right',
+    MIDDLE_LEFT = 'middle left',
+    MIDDLE = 'middle',
+    MIDDLE_RIGHT = 'middle right',
+    BOTTOM_LEFT = 'bottom left',
+    BOTTOM_CENTER = 'bottom center',
+    BOTTOM_RIGHT = 'bottom right',
+}
+
+type AlertTypesString = 'info' | 'success' | 'error'
+
+enum AlertTypes {
+    INFO = 'info',
+    SUCCESS = 'success',
+    ERROR = 'error',
+}
+
+export {}
+
+type TransitionsString = 'fade' | 'scale'
+enum Transitions {
+    FADE = 'fade',
+    SCALE = 'scale',
+}
+
+export { Positions, AlertTypes, Transitions }
+
+interface Options {
+    position?: Positions | PositionsString
+    type?: AlertTypes | AlertTypesString
+    transition?: Transitions | TransitionsString
+    timeout?: number
+    containerStyle?: CSSProperties
+
+    // offset: string,
+
+    // template: PropTypes.oneOfType([
+    // PropTypes.element,
+    // PropTypes.func,
+    // PropTypes.elementType
+    // ]).isRequired,
+    // context: PropTypes.shape({
+    // Provider: PropTypes.object,
+    // Consumer: PropTypes.object
+    // })
+}
+
+export { Options }
---
+++ src/types/props.ts
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+
+import { Options } from './options'
+
+// import {}
+
+interface ProviderProps {
+    children?: ReactNode
+    options?: Options
+}
+
+export { ProviderProps }
