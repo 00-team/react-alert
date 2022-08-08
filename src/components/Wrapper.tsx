f3ef5956bf434f777195656347e7c967a4fe4d44
007 <dr007cc@gmail.com>
2022-08-08 22:56:30 +0430

    feat: Transition Wrapper

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,84 @@
+import React, {
+    CSSProperties,
+    FC,
+    HTMLAttributes,
+    ReactNode,
+    useMemo,
+} from 'react'
+
+import { Positions, PositionsString } from '../types'
+
+const DefaultStyles: CSSProperties = {
+    left: 0,
+    position: 'fixed',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    width: '100%',
+    pointerEvents: 'none',
+}
+
+type PS = Record<PositionsString, CSSProperties>
+const PosStyles: PS = {
+    [Positions.TOP_LEFT]: {
+        top: 0,
+        alignItems: 'flex-start',
+    },
+    [Positions.TOP_CENTER]: {
+        top: 0,
+    },
+    [Positions.TOP_RIGHT]: {
+        top: 0,
+        alignItems: 'flex-end',
+    },
+    [Positions.MIDDLE_LEFT]: {
+        top: '50%',
+        alignItems: 'flex-start',
+    },
+    [Positions.MIDDLE]: {
+        top: '50%',
+    },
+    [Positions.MIDDLE_RIGHT]: {
+        top: '50%',
+        alignItems: 'flex-end',
+    },
+    [Positions.BOTTOM_LEFT]: {
+        bottom: 0,
+        alignItems: 'flex-start',
+    },
+    [Positions.BOTTOM_CENTER]: {
+        bottom: 0,
+    },
+    [Positions.BOTTOM_RIGHT]: {
+        bottom: 0,
+        alignItems: 'flex-end',
+    },
+}
+
+interface WrapperProps extends HTMLAttributes<HTMLDivElement> {
+    children: ReactNode[]
+    position: PositionsString
+    containerStyle: CSSProperties
+}
+
+const Wrapper: FC<WrapperProps> = props => {
+    const { children, position, containerStyle, ...attrs } = props
+    const PosStyle = useMemo(() => PosStyles[position], [position])
+
+    if (children.length < 1) return <></>
+
+    const style = {
+        ...DefaultStyles,
+        ...PosStyle,
+        ...containerStyle,
+    }
+
+    return (
+        <div {...attrs} style={style}>
+            {children}
+        </div>
+    )
+}
+
+export { Wrapper }
